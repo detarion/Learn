bfe6b2ec26e81344a3e3a4e2de6e27a1a5bb8795
User <test@example.com>
2025-11-11 02:14:15 +0700

    ООП: виртуальные функции

--- main.cpp
+++ main.cpp
@@ -467,18 +467,108 @@ void TaskOOP1()
     // ~A()
 }
 
+void TaskOOP2()
+{
+    struct A
+    {
+        A()
+        {
+            std::cout << "A()" << std::endl;
+        }
+
+        virtual ~A()
+        {
+            std::cout << "~A()" << std::endl;
+        }
+
+        void Function()
+        {
+            Print();
+        }
+
+        virtual void Print()
+        {
+            std::cout << "A::Print()" << std::endl;
+        }
+    };
+        
+    struct B : A
+    {
+        B()
+        {
+            std::cout << "B()" << std::endl;
+        }
+
+        ~B()
+        {
+            std::cout << "~B()" << std::endl;
+        }
+
+        void Function()
+        {
+            Print();
+        }
+
+        void Print() override
+        {
+            std::cout << "B::Print()" << std::endl;
+        }
+    };
+
+    struct C : B
+    {
+        C()
+        {
+            std::cout << "C()" << std::endl;
+            A::Function();
+        }
+
+        ~C()
+        {
+            std::cout << "~C()" << std::endl;
+        }
+
+        void Function()
+        {
+            Print();
+        }
+
+        void Print() override
+        {
+            std::cout << "C::Print()" << std::endl;
+        }
+    };
+
+    A* p = new C();
+   
+    std::cout << "..." << std::endl;
+
+    delete p;
+    p = nullptr;
+
+    // A()
+    // B()
+    // C()
+    // C::Print()
+    // ...
+    // ~C()
+    // ~B()
+    // ~A()
+}
+
 void OOP()
 {
-    TaskOOP1();
+    //TaskOOP1();
+    TaskOOP2();
 }
 
 int main()
 {
     std::setlocale(LC_ALL, "");
 
-    Containers();
-    Algorithms();
-    Sorts();
+    // Containers();
+    // Algorithms();
+    // Sorts();
     OOP();
 
     return 0;
